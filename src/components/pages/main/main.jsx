735dcf202020304be02070b5af61c741b7ae0042
Witcher-MTM <phantomit228@gmail.com>
2024-03-01 17:52:22 +0200

    add main content of page

--- src/components/pages/main/main.jsx
+++ src/components/pages/main/main.jsx
@@ -1,26 +1,34 @@
 import React from "react";
-
+import './main.css'
 const Main = () => {
   return (
     <div>
-      <div style={styles.container}>
+      <div className="backgroundImage">
+      <div className="header">
         <div>
-          Image
+          Logo
         </div>
-        <div>главная услуги отзывы</div>
+        <div className="headertext">
+          <h6>ГЛАВНАЯ</h6>
+          <h6>УСЛУГИ</h6>
+          <h6>ОТЗЫВЫ</h6>
+        </div>
       </div>
-      Main
+      <div className="maintext">
+        <h1>ЧАСТНЫЙ КИНОТЕАТР LUMINAPLEX</h1>
+        <h3>ОРГАНИЗУЕМ ЯРКИЙ МИНЕТОСИК</h3>
+        <h6>ДЛЯ ДВОИХ • ДЛЯ КОМПАНИИ • ДЕВИЧНИКИ</h6>
+        <button><strong>СТАТЬ МАМОНТОМ</strong></button>
+      </div>
     </div>
+    <div className="content">
+      <h3>УСЛУГИ</h3>
+    </div>
+    </div>
+
   )
 }
 const styles = {
-  container: {
-    backgroundColor: 'rgba(0, 0, 0, 0.8)',
-    height: '8vh',
-    display: 'flex',
-    justifyContent: 'space-between', 
-    alignItems: 'center',
-    padding: '0 250px'
-  }
+
 };
 export default Main
